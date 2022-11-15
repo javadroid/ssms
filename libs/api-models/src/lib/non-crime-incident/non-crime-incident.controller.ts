d6135c18e8ff4714a921fdfc34526b682443a50e
Teddy9990 <Mbatoteddy@gmail.com>
2022-11-15 11:50:28 +0100

    teddy updates

---
+++ libs/api-models/src/lib/non-crime-incident/non-crime-incident.controller.ts
@@ -0,0 +1,53 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    NotFoundException,
+    Param,
+    Patch,
+    Post,
+  } from '@nestjs/common';
+
+  import { NonCrimeIncidentDto } from '../../dto/noncrimeincident.dto';
+  import { NonCrimeIncidentDoc } from '../../schema/noncrimeincident.schema';
+  import { NonCrimeIncidentService } from './non-crime-incident.service';
+
+@Controller('non-crime-incident')
+export class NonCrimeIncidentController {
+    constructor(private nonCrimeIncidentService: NonCrimeIncidentService) {}
+  @Post()
+  async create(@Body() createNonCrimeIncident: NonCrimeIncidentDoc) {
+    return this.nonCrimeIncidentService.create(createNonCrimeIncident);
+  }
+
+  @Get()
+  findAll() {
+    return this.nonCrimeIncidentService.findAll();
+  }
+
+  @Get(':id')
+  async findbyId(@Param('id') id: string) {
+    return this.nonCrimeIncidentService.findbyId(id);
+  }
+
+  @Get(':id/:value')
+  async findbyAny(@Param('id') id: string, @Param('value') value: string) {
+    if (id === 'evidence' || id === 'reportDetail' || id === 'reporterId') {
+      return this.nonCrimeIncidentService.findbyAny(id, value);
+    } else {
+      throw new NotFoundException("fleid '" + id + "' not found");
+    }
+  }
+
+  @Patch(':_id')
+  async update(@Param('_id') _Id: string, @Body() updated: NonCrimeIncidentDto) {
+    return this.nonCrimeIncidentService.update(_Id, updated);
+  }
+
+  @Delete(':_id')
+  async delete(@Param('_id') _Id: string) {
+    return this.nonCrimeIncidentService.delete(_Id);
+  }
+
+}
