197e503151af44cfe4db39d902e4e636cf6ff0c9
AsukFavour <favourasuk@gmail.com>
2022-11-23 13:38:56 +0100

    asuk

--- libs/ssms-frontend/src/index.ts
+++ libs/ssms-frontend/src/index.ts
@@ -2,10 +2,8 @@ export * from './lib/report/report.module';
 export * from './lib/policy-info/policy-info.module';
 export * from './lib/dashboard/dashboard.module';
 export * from './lib/new-report/new-report.module';
-<<<<<<< HEAD
 export * from './lib/tabs/tabs.module';
 export * from './lib/indexs/indexs.module';
-=======
 export * from './lib/sign-up/sign-up.module';
 export * from './lib/login/login.module';
->>>>>>> 783fc9b4d29c634acdbab640bada37d438f487fd
+
