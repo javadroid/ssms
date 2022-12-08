d5ca450ecaad3eff425d733415d2387facbd84ae
Javadroid <ochigboemmanuel32@gmail.com>
2022-12-08 02:17:32 +0100

    a

--- libs/api-models/src/lib/personnel/auth/strategy/jwt.strategy.ts
+++ libs/api-models/src/lib/personnel/auth/strategy/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_CONSTANT_PRE,
+      secretOrKey: process.env.JWT_CONSTANT_PER,
     });
   }
 
