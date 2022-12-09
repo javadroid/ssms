17626ab85fd6030c3b0269b1ce6f48d3aebe5ebd
hanni-dev <kariebi.akpeki@gmail.com>
2022-12-09 16:32:36 +0100

    a

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -16,7 +16,6 @@ import { ConfigModule } from '@nestjs/config';
 import { FormsModule } from '@angular/forms';
 import { RefModule } from '@ssms/ref';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
